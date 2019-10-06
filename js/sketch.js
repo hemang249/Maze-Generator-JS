789d2610048160009c5259a270190deac0f0ccb8
Hemang Bhogayata <hemang249@gmail.com>
2019-10-07 01:54:49 +0530

    Recursive Backtracing Implemented

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@ var cols , rows;
 var w = 40;
 var grid = []
 var current = null
+var stack = []
 
 function setup(){
     createCanvas(400,400)
@@ -23,13 +24,19 @@ function draw(){
     for(let i = 0 ; i < grid.length ; i++){
         grid[i].show()
     }
-
+    
     current.visited = true
-    var nextCell = current.checkNeighbours()
+    current.highlight()
+    var nextCell = current.checkNeighbors()
 
     if(nextCell){
         nextCell.visited = true
+        stack.push(current)
+        removeWalls(current , nextCell)
         current = nextCell
+    } else if(stack.length !== 0) {
+        var cell = stack.pop()
+        current = cell
     }
 }
 
@@ -39,61 +46,95 @@ function getIndex(i , j){
     return i + j * cols
 }
 
-function Cell(i , j){
-    this.i = i
-    this.j = j
-    this.walls = [true , true , true , true]   // top right bottom left 
-    this.visited = false
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+    this.walls = [true, true, true, true];
+    this.visited = false;
+  
+    this.checkNeighbors = function() {
+      var neighbors = [];
+  
+      var top    = grid[getIndex(i, j -1)];
+      var right  = grid[getIndex(i+1, j)];
+      var bottom = grid[getIndex(i, j+1)];
+      var left   = grid[getIndex(i-1, j)];
+  
+      if (top && !top.visited) {
+        neighbors.push(top);
+      }
+      if (right && !right.visited) {
+        neighbors.push(right);
+      }
+      if (bottom && !bottom.visited) {
+        neighbors.push(bottom);
+      }
+      if (left && !left.visited) {
+        neighbors.push(left);
+      }
+  
+      if (neighbors.length > 0) {
+        var r = floor(random(0, neighbors.length));
+        return neighbors[r];
+      } else {
+        return undefined;
+      }
+  
+  
+    }
+    this.highlight = function() {
+      var x = this.i*w;
+      var y = this.j*w;
+      noStroke();
+      fill(0, 0, 255, 100);
+      rect(x, y, w, w);
+  
+    }
+  
+    this.show = function() {
+      var x = this.i*w;
+      var y = this.j*w;
+      stroke(255);
+      if (this.walls[0]) {
+        line(x    , y    , x + w, y);
+      }
+      if (this.walls[1]) {
+        line(x + w, y    , x + w, y + w);
+      }
+      if (this.walls[2]) {
+        line(x + w, y + w, x    , y + w);
+      }
+      if (this.walls[3]) {
+        line(x    , y + w, x    , y);
+      }
+  
+      if (this.visited) {
+        noStroke();
+        fill(255, 0, 255, 100);
+        rect(x, y, w, w);
+      }
+    }
+  }
 
-    this.checkNeighbours = function(){
-        var neighbours = []
-        var top = grid[getIndex(i , j-1)]
-        var right = grid[getIndex(i+1,j)]
-        var bottom = grid[getIndex(i,j+1)]
-        var left = grid[getIndex(i-1,j)]
+function removeWalls(a,b){
+    var diffX = a.i - b.i
 
-        if(top && !top.visited)
-            neighbours.push(top)
-        if(right && !right.visited)
-            neighbours.push(right)
-        if(right && !bottom.visited)
-            neighbours.push(bottom)
-        if(left && !left.visited)
-            neighbours.push(left)
-
-        if(neighbours.length > 0){
-            var r = floor(random(0,neighbours.length))
-            return neighbours[r]
-        } else {
-            return undefined
-        }
+    if(diffX === 1){
+        a.walls[3] = false
+        b.walls[1] = false
+    } else {
+        a.walls[1] = false
+        b.walls[3] = false
     }
 
-    this.show = function(){
-        let x  = this.i * w
-        let y = this.j * w
-        stroke(255)
+    var diffY = a.j - b.j
 
-        if(this.walls[0]){
-            line(x, y, x+w,y)
-        }
-
-        if(this.walls[1]){
-            line(x+ w, y,x+w,y+w)
-        }
-
-        if(this.walls[2]){
-            line(x+w, y+w,x,y+w)
-        }
-
-        if(this.walls[3]){
-            line(x, y, x,y+w)
-        }
-
-        if(this.visited){
-            fill(255, 0 , 255 , 100);
-            rect(x,y,w,w)
-        }
-
+    if(diffY === 1){
+        a.walls[0] = false
+        b.walls[2] = false
+    } else {
+        a.walls[2] = false
+        b.walls[0] = false
     }
+    
 }
